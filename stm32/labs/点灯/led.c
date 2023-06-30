5eee3dd5976dca3b935926014fb1556e0aa140eb
Leavaway <86211987+Leavaway@users.noreply.github.com>
2023-06-30 22:17:29 +0800

    Update led.c

--- stm32/labs/点灯/led.c
+++ stm32/labs/点灯/led.c
@@ -26,3 +26,19 @@ void LED2_ON(void){
 void LED2_OFF(void){
 	GPIO_SetBits(GPIOA, GPIO_Pin_2);
 }
+
+void LED1_Turn(void){
+	if(GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_1)==0){
+		GPIO_SetBits(GPIOA, GPIO_Pin_1);
+	}else{
+		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+	}
+}
+
+void LED2_Turn(void){
+	if(GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_2)==0){
+		GPIO_SetBits(GPIOA, GPIO_Pin_2);
+	}else{
+		GPIO_ResetBits(GPIOA, GPIO_Pin_2);
+	}
+}
